aec8765068a5d8916b580e3bcedd54ec17512089
mrutyunjay mahanta <princemrutyunjay6@gmail.com>
2020-12-13 21:03:18 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+var btn= document.getElementById("btn").addEventListener("click",getPost);
+var con=0;
+var div= document.getElementById("carDiv");
+
+function getPost(){
+    fetch('https://jsonplaceholder.typicode.com/photos')
+    .then((res)=>{
+        return res.json();
+    })
+    .then((post)=>{
+        for (let index =0; index < 1; index++) {
+           div.innerHTML+=`
+              <div class="card col-3 m-1 mx-auto">
+                    <img class="card-img-top" src="${post[con].thumbnailUrl}"></img>
+                   <div class="card-body">
+                       <h5 class="cart-title">${post[con].id}</h5>
+                       <p class="card-text">${post[con].title}</p>
+
+                   </div>
+
+                </div> 
+
+            ` 
+            con=con+1;
+           
+        }
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+}
